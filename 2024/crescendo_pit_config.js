195f12eccfc6fe18e253cd68352fc3a0922dcb09
kmcnamara40 <34079529+kmcnamara40@users.noreply.github.com>
2024-02-21 22:16:21 -0500

    Update crescendo_pit_config.js

--- 2024/crescendo_pit_config.js
+++ 2024/crescendo_pit_config.js
@@ -36,24 +36,12 @@ var config_data = `
       "size": 20,
       "maxSize": 50
     },
-    { "name": "Swerve Ratio",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "L1 (8.14:1)<br>",
-        "2": "L2 (6.75:1)<br>",
-        "3": "L3 (6.12:1)<br>",
-        "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
-    },
     { "name": "Drivetrain Motor",
       "code": "mot",
       "type": "radio",
       "choices": {
         "n": "Neo<br>",
+        "v": "Vortex<br>",
         "f": "Falcon<br>",
         "k": "Kraken<br>",
         "c": "CIM<br>",
