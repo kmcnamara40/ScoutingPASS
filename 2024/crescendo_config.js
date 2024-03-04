f3e8f201e3bea74dc8f038f7a6f48b0ddb9787d8
kmcnamara40 <34079529+kmcnamara40@users.noreply.github.com>
2024-03-04 08:27:06 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024flwp",
+      "defaultValue": "2024tnkn",
       "required": "true"
     },
     { "name": "Match Level",
