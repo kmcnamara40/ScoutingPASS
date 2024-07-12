f1afdbc865bc8bec7c1ea2e9e941e27bdfeeedaf
kmcnamara40 <34079529+kmcnamara40@users.noreply.github.com>
2024-07-11 23:20:54 -0400

    Update crescendo_config.js
    changed code for iri

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024alhu",
+      "defaultValue": "2024iri",
       "required": "true"
     },
     { "name": "Match Level",
